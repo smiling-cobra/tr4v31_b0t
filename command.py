74ff21779ec89e503f494034680cd1ca81b35863
Jenya Golovnov <93ave.blues@gmail.com>
2023-08-04 00:49:51 +0200

    get_tourist_attractions / refactoring

--- command.py
+++ command.py
@@ -1,7 +1,10 @@
+import os
+import logging
+import requests
+from datetime import date
 from abc import ABC, abstractmethod
 from telegram import Update, KeyboardButton, ReplyKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, ConversationHandler
-import logging
 
 # # Configure the logging settings
 # logging.basicConfig(
@@ -10,6 +13,10 @@ import logging
 #     datefmt='%Y-%m-%d %H:%M:%S'
 # )
 
+client_id = os.environ.get('FOURSQUARE_CLIENT_ID')
+client_secret = os.environ.get('FOURSQUARE_CLIENT_SECRET')
+base_url = os.environ.get('FOURSQSARE_API_URL')
+
 class Command(ABC):
     @abstractmethod
     def execute(self, update: Update, context: CallbackContext) -> None:
@@ -17,11 +24,41 @@ class Command(ABC):
 
 class TouristAttractionsCommand(Command):
     def execute(self, update: Update, context: CallbackContext) -> None:
+        city_name = self.get_city_name(context)
+        tourist_attractions = self.get_tourist_attractions(city_name)
+        print(tourist_attractions)
+        update.message.reply_text(f"Here are some popular tourist attractions in {city_name}:")
+        pass
+    
+    def get_city_name(self, context: CallbackContext) -> str:
         city_data = context.user_data.get('city_data')[0]
         address_components = city_data.get('address_components')[0]
         city_name = address_components.get('long_name')
-        update.message.reply_text(f"Here are some popular tourist attractions in {city_name}:")
-        pass
+        return city_name
+    
+    def get_tourist_attractions(self, city_name: str) -> list:
+        # Your code to get tourist attractions
+        version = date.today().strftime("%Y%m%d")
+
+        params = {
+            'client_id': client_id,
+            'client_secret': client_secret,
+            'v': version,
+            'near': city_name,
+            'query': 'tourist_attraction',
+            'limit': 5
+        }
+
+        # Usage of the V2 Places API has been deprecated for new Projects
+        # https://docs.foursquare.com/reference
+        response = requests.get(base_url, params=params)
+
+        if response.status_code == 200:
+            data = response.json()
+            venues = data.get('response').get('venues')
+            return venues
+        else:
+            return []
 
 class WeatherForecastCommand(Command):
     def execute(self, update: Update, context: CallbackContext) -> None:
