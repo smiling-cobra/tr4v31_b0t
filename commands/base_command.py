ce7a09d8b33948ff2dff5c5fc08be2ac9fc9834d
jenya golovnov <93ave.blues@gmail.com>
2024-01-27 16:47:51 +0000

    base_command.py indentation fix

--- commands/base_command.py
+++ commands/base_command.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 from telegram import Update
 from telegram.ext import CallbackContext
 
+
 class Command(ABC):
     @abstractmethod
     def execute(self, update: Update, context: CallbackContext) -> None:
